0f0b11d7b822f0ec4f233133749783e228e3a06c
FelipeAndriFe <felipeaferreira06@gmail.com>
2026-04-25 21:04:35 -0300

    ajustes

--- Trabalho1/Trabalho1.c
+++ Trabalho1/Trabalho1.c
@@ -8,13 +8,13 @@ FindDiagonalOrder
     Returns the vector of a given matrix read in diagonal order
 ======================
 */
-int* findDiagonalOrder( int** mat, int matSize, int* matColSize, int* returnSize ) {
+int* FindDiagonalOrder( int** mat, int matSize, int* matColSize, int* returnSize ) {
     int *result, k, c = 0, diagonals, i = 0, j = 0;
 
     diagonals = ( matSize + ( *matColSize ) ) - 1;
     *returnSize = ( matSize * ( *matColSize ) );
 
-    result = ( int * )malloc( sizeof(int) * ( matSize * ( *matColSize ) ) );
+    result = ( int * )malloc( sizeof( int ) * ( matSize * ( *matColSize ) ) );
     if ( !result ) {
         return NULL;
     }
